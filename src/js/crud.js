0b27e90b22247eddc95d04bd1e07a5c61dbd3742
Ihor849 <333marchelo333@gmail.com>
2023-03-16 01:33:25 +0200

    закінчення async/await

--- src/js/crud.js
+++ src/js/crud.js
@@ -57,18 +57,6 @@
 // removeBook(15);
 // removeBook(14);
 
-// function updateBookById(update, bookId) {
-//   const options = {
-//     method: 'PATCH',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(update),
-//   };
-
-//   return fetch(`${BASE_URL}/books/${bookId}`, options).then(res => res.json());
-// }
-
 // updateBookById({ title: 'Большая новая книга по NODEJS' }, 19);
 // updateBookById({ rating: 1 }, 18);
 // updateBookById({ rating: 4, author: 'Манго' }, 17);
